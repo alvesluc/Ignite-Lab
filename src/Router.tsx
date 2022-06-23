c5dede24a4a7e5b4ddeab0967a8d3a4ffa05f0b1
Lucas Alves <l.alves001@gmail.com>
2022-06-23 20:51:11 -0300

    Aula 4

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Subscribe />} />
+      <Route path="/event" element={<Event />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
   );
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { format, isPast } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -11,6 +11,7 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
+  const { slug } = useParams<{ slug: string }>();
   const isLessonAvailable = isPast(props.availableAt);
   const availableDateFormatted = format(
     props.availableAt,
@@ -18,14 +19,24 @@ export function Lesson(props: LessonProps) {
     { locale: ptBR }
   );
 
+  const isActiveLesson = slug === props.slug;
+
   return (
     <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
+      <div
+        className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors ${
+          isActiveLesson && "bg-green-500"
+        }`}
+      >
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
-            <span className="text-sm text-blue-500 font-medium flex item-center gap-2">
+            <span
+              className={`text-sm font-medium flex item-center gap-2 
+              ${!isActiveLesson && "text-blue-500"}
+              ${isActiveLesson && "text-white"}`}
+            >
               <CheckCircle size={20} />
               Conteúdo liberado
             </span>
@@ -36,12 +47,21 @@ export function Lesson(props: LessonProps) {
             </span>
           )}
 
-          <span className="text-xs  px-2 py-[0.125rem] text-white rounded border border-green-300 font-bold">
+          <span
+            className={`text-xs  px-2 py-[0.125rem] text-white rounded border border-green-300 font-bold 
+            ${isActiveLesson && "border-white"}`}
+          >
             {props.type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
           </span>
         </header>
 
-        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
+        <strong
+          className={`mt-5 block 
+          ${!isActiveLesson && "text-gray-200"} 
+          ${isActiveLesson && "text-white"}`}
+        >
+          {props.title}
+        </strong>
       </div>
     </Link>
   );
